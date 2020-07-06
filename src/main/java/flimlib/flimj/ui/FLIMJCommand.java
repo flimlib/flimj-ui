0194179104d2ea771619e95f9534a2e28d51338d
Curtis Rueden <ctrueden@wisc.edu>
2020-07-06 12:56:19 -0500

    Add runAndWait helper method for JavaFX
    We will need to initialize JavaFX stuff, wait till it's done, and
    only then hand the fully initialized objects to Swing for display.
    This method will make it easier to do that elegantly.

--- src/main/java/flimlib/flimj/ui/FLIMJCommand.java
+++ src/main/java/flimlib/flimj/ui/FLIMJCommand.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -61,6 +62,43 @@ public class FLIMJCommand implements Command {
 				throw new RuntimeException(e);
 			}
 		});
+	}
+
+	/**
+	 * Runs the specified {@link Runnable} on the
+	 * JavaFX application thread and waits for completion.
+	 * <p>
+	 * Credit:
+	 * <a href="https://news.kynosarges.org/2014/05/01/simulating-platform-runandwait/">Christoph Nahr</a>
+	 * </p>
+	 *
+	 * @param action the {@link Runnable} to run
+	 * @throws NullPointerException if {@code action} is {@code null}
+	 */
+	private void runAndWait(final Runnable action) {
+		// run synchronously on JavaFX thread
+		if (Platform.isFxApplicationThread()) {
+			action.run();
+			return;
+		}
+
+		// queue on JavaFX thread and wait for completion
+		final CountDownLatch doneLatch = new CountDownLatch(1);
+		Platform.runLater(() -> {
+			try {
+				action.run();
+			}
+			finally {
+				doneLatch.countDown();
+			}
+		});
+
+		try {
+			doneLatch.await();
+		}
+		catch (final InterruptedException e) {
+			log().error(e);
+		}
 	}
 
 	private Logger log() {
