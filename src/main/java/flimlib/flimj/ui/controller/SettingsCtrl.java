37193856b5cfc7632f64603952c8c6ac89a31164
Curtis Rueden <ctrueden@wisc.edu>
2025-01-29 14:55:51 -0600

    Update license blurbs for 2025

--- src/main/java/flimlib/flimj/ui/controller/SettingsCtrl.java
+++ src/main/java/flimlib/flimj/ui/controller/SettingsCtrl.java
@@ -2,7 +2,7 @@
  * #%L
  * Fluorescence lifetime analysis in ImageJ.
  * %%
- * Copyright (C) 2019 - 2022 Board of Regents of the University of Wisconsin-Madison.
+ * Copyright (C) 2019 - 2025 Board of Regents of the University of Wisconsin-Madison.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
